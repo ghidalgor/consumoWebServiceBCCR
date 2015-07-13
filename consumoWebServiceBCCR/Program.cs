8361636db32ef94bbcdaec3ab546ae304cf92137
ghidalgor <ghidalgor@gmail.com>
2015-07-13 10:32:43 -0600

    First files to upload.

---
+++ consumoWebServiceBCCR/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace consumoWebServiceBCCR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cr.fi.bccr.indicadoreseconomicos.wsIndicadoresEconomicos cliente = new cr.fi.bccr.indicadoreseconomicos.wsIndicadoresEconomicos();
+            DataSet tipoCambio = cliente.ObtenerIndicadoresEconomicos("317", "15/11/2014","15/11/2014", "Gregory Hidalgo", "N");
+            Console.WriteLine("Código Indicador: "+ tipoCambio.Tables[0].Rows[0].ItemArray[0].ToString());
+            Console.WriteLine("Fecha Consulta: " + tipoCambio.Tables[0].Rows[0].ItemArray[1].ToString());
+            Console.WriteLine("Valor tipo cambio: " + tipoCambio.Tables[0].Rows[0].ItemArray[2].ToString());
+            Console.ReadLine();
+        }
+    }
+}
